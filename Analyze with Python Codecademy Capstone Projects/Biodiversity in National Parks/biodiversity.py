0081d3a7053f33168e8f146077ab27c45924b63d
andayduman <44687213+andayduman@users.noreply.github.com>
2020-07-10 20:47:00 -0400

    Update biodiversity.py

--- Analyze with Python Codecademy Capstone Projects/Biodiversity in National Parks/biodiversity.py
+++ Analyze with Python Codecademy Capstone Projects/Biodiversity in National Parks/biodiversity.py
@@ -2,17 +2,29 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from scipy.stats import chi2_contingency
 
-species = pd.read_csv('species_info.csv')
+#data from csv files used in analysis contain mock data based on real data from National Parks Service in USA
+
+species = pd.read_csv('species_info.csv') 
+#loading in csv file containing info on species found within parks
 species.head()
 
-species.scientific_name.nunique()
+species.scientific_name.nunique() 
+#calculating number of species in dataset: 5541 species
 
-species.category.unique()
+species.category.unique() 
+#determining the different categories of species in dataset: Mammal, Bird, Reptile, Amphibian, Fish, Vascular Plant, Nonvascular plant
 
 species.conservation_status.unique()
+#determing differet values in conservation_status column: nan, Species of Concern, Endangered, Threatened, In Recovery
+
+species.groupby('conservation_status').scientific_name.nunique().reset_index()
+#creating table to group each unique scientific_name (species) category within the different conservation statuses
 
 species.fillna('No Intervention', inplace=True)
+#replacing 'None' with 'No Intervention'
+
 species.groupby('conservation_status').scientific_name.nunique().reset_index()
+#creating table from species dataframe listing the different conservation statuses and how many unique scientific names (species) are categorized in each status
 
 protection_counts = species.groupby('conservation_status').scientific_name.nunique().reset_index().sort_values(by='scientific_name')
 
@@ -34,6 +46,7 @@ category_counts.head(10)
 category_pivot = category_counts.pivot(columns='is_protected', index='category', values='scientific_name').reset_index()
 
 category_pivot.columns = ['category', 'not_protected', 'protected']
+              
 category_pivot['percent_protected'] = category_pivot.protected / (category_pivot.protected + category_pivot.not_protected)
 category_pivot.head(10)
 
@@ -48,9 +61,13 @@ observations.head(6)
 
 species['is_sheep'] = species.common_names.apply(lambda x: 'Sheep' in x)
 species.head()
+              
 species[species.is_sheep]
+              
 sheep_species = species[(species.is_sheep) & (species.category == 'Mammal')]
+              
 sheep_species = species[(species.is_sheep) & (species.category == 'Mammal')]
+              
 sheep_observations = observations.merge(sheep_species)
 
 sheep_obs_by_park = sheep_observations.groupby('park_name').observations.sum().reset_index()
